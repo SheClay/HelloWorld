79876ad48ed7661d6b9bfb9b5561b84941751982
Marie Clay <mclay2007@gmail.com>
2020-01-14 14:22:12 -0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System; 
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("It's a beautiful day!");
+            Console.ReadLine();
+          
+
+            
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
